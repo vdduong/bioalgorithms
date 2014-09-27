2b23ebe95d7c2946bfc28d33faa46755b2cd2b52
vdduong <vdduong@free.fr>
2014-09-28 01:07:12 +0200

    Update hai.py

--- hai.py
+++ hai.py
@@ -7,22 +7,17 @@ def convert(A,B):
 # base case A = 1, B= 0 so thqt 2 positions
 # base case A = 0, B= 1 so thqt 2 positions
 
-dict_nb_positions = dict()
-dict_nb_positions[(0,0)] = 1
-dict_nb_positions[(0,1)] = 2
-dict_nb_positions[(1,0)] = 2
-
-dict_positions = dict()
-
-# given A, B
-n_a, n_b = convert(A,B)
-
-def positions(n_a, n_b):
-  if n_a = 0 and n_b = 0:
-    nb_positions = 1
-    positions[nb_positions] = (0,0)
-    return nb_positions, positions
+dict_last_positions = dict() # hash table
+def last_positions():
+  if n_a, n_b == 0, 0:
+    dict_last_positions[(0,0)]= (0,0)
   else:
-    min_ab = min(n_a, n_b)
-    nb_positions, positions = positions(n_a-1, n_b)
-    
+    next_step = min(n_a,n_b)
+    for key_step in dict_last_positions:
+      last_positions = dict_last_positions[key_step]
+      if last_positions[0]+2**next_step <= A:
+        last_positions_0 = last_positions[0] + 2**next_step
+        del dict_last_positions[key_step] # remove old last positions
+        dict_last_positions[]
+      elif last_positions[1] + 2**next_step <= B:
+        last_positions_1 = last_positions[0] + 2**next_step
