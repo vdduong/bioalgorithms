197fbf2a61fc7e9fc56fb10f846cb51b8fb6f157
vdduong <vdduong@free.fr>
2015-03-11 12:43:29 +0100

    Update iterameta.py

--- iterameta.py
+++ iterameta.py
@@ -1,1 +1,17 @@
 from Tkinter import *
+
+# import routines or something else
+
+MyWindow = Tk()
+MyWindow.title('ITERAMETA')
+MyWindow.geometry('300x100+400+400')
+
+ButtonTOCSY = Button(MyWindow, text='TOCSY peak list',command='')
+ButtonTOCSY.pack(side=LEFT,padx=5, pady=5)
+
+ButtonQuit = Button(MyWindow,text='Quit',command='')
+ButtonQuit.pack(side=LEFT,padx=5,pady=5)
+
+Text = StringVar()
+
+MyWindow.mainloop()
